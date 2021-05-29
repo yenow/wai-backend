96bccf125c1454992670d3585184b81f709d96c9
sinyoung yun <54181528+yenow@users.noreply.github.com>
2021-05-30 02:32:49 +0900

    Initial commit

---
+++ src/test/java/com/zootopia/zootopiaspring/ZootopiaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zootopia.zootopiaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZootopiaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
